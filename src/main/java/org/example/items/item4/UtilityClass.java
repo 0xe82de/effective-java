25b1f8a717de8b698bb1e0215ca78df41c93fb24
김경석 <0xe82de@gmail.com>
2023-01-28 23:13:30 +0900

    update: 아이템 4 인스턴스화를 막으려거든 private 생성자를 사용하라

---
+++ src/main/java/org/example/items/item4/UtilityClass.java
@@ -0,0 +1,11 @@
+package org.example.items.item4;
+
+public class UtilityClass {
+
+    private UtilityClass() {
+        /**
+         * 클래스 안에서 생성자를 호출하지 않도록 한다.
+         */
+        throw new AssertionError();
+    }
+}
